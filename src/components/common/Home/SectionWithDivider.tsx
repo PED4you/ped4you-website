d1ac08e119dfbf37acc08dd6ffbbaf23449e4304
pryter <peterphongpak@gmail.com>
2023-04-18 16:53:57 +0700

    fix(ui): divider width

--- src/components/common/Home/SectionWithDivider.tsx
+++ src/components/common/Home/SectionWithDivider.tsx
@@ -4,8 +4,8 @@ import {FC, ReactNode} from "react"
 export const SectionWithDivider: FC<{children: ReactNode}> = ({children}) => {
   return <section className="max-w-desktop mx-auto">
     {children}
-    <div className="w-full px-10 md:px-64">
-      <div className="border-b-[2px]  border-PED-green"/>
+    <div className="w-full px-10 sm:px-[10vw]">
+      <div className="border-b-[2px] border-PED-green sm:mx-auto sm:max-w-[640px]"/>
     </div>
   </section>
 }
