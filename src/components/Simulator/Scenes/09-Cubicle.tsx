8f6ee31f9121341ed649726cfdc6ba94dc9f8d4c
betich <betich365@gmail.com>
2023-05-01 19:19:55 +0700

    fix: remove text from ballot page

--- src/components/Simulator/Scenes/09-Cubicle.tsx
+++ src/components/Simulator/Scenes/09-Cubicle.tsx
@@ -49,7 +49,7 @@ export default function Cubible() {
 
   return (
     <section className="relative mx-auto flex min-h-screen w-full max-w-xl flex-col items-center justify-center overflow-hidden">
-      <Heading />
+      {type !== "ballot" && <Heading />}
 
       <div className="relative">
         <AnimatePresence>
@@ -161,7 +161,7 @@ export default function Cubible() {
               animate={{ y: 0, opacity: 1 }}
               exit={{ y: -300, opacity: 0 }}
               transition={{
-                // delay: 0.5,
+                delay: 0.5,
                 duration: 0.5,
                 staggerChildren: 0.5,
               }}
--- src/components/Simulator/constants.ts
+++ src/components/Simulator/constants.ts
@@ -1,2 +1,2 @@
 export const DEBUG = false
-export const DEBUG_PAGE = "4"
+export const DEBUG_PAGE = "8"
